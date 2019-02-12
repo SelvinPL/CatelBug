79c569f97ab40554e1bf12028f60bdeeb2b188cb
SelvinPL <przemek.sulikowski@gmail.com>
2019-02-12 17:15:20 +0100

    Revert "error in with 5.9"
    This reverts commit 4247816f6d30028cf23ef3425efe3e77ead929b1.

--- CatelBug/MVVM/MyFancyView.cs
+++ CatelBug/MVVM/MyFancyView.cs
@@ -3,7 +3,6 @@
     using Catel.MVVM;
     using Catel.Windows.Controls;
     using System;
-    using System.Diagnostics;
     using System.Windows.Media;
 
     public class MyFancyView : UserControl
@@ -22,16 +21,9 @@
 
         private void Init()
         {
-            try
-            {
-                var grid = new System.Windows.Controls.Grid();
-                grid.Background = Brushes.Red;
-                AddChild(grid);
-            } catch (Exception ex)
-            {
-                Console.WriteLine("==Exception in Init() {0}, trace: {1}==", ex.Message, ex.StackTrace);
-                throw;
-            }
+            var grid = new System.Windows.Controls.Grid();
+            grid.Background = Brushes.Red;
+            AddChild(grid);
         }
 
         protected override void OnInitialized(EventArgs e)
