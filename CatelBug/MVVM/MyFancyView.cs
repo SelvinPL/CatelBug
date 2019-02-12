4247816f6d30028cf23ef3425efe3e77ead929b1
SelvinPL <przemek.sulikowski@gmail.com>
2019-02-12 13:54:05 +0100

    error in with 5.9

--- CatelBug/MVVM/MyFancyView.cs
+++ CatelBug/MVVM/MyFancyView.cs
@@ -3,6 +3,7 @@
     using Catel.MVVM;
     using Catel.Windows.Controls;
     using System;
+    using System.Diagnostics;
     using System.Windows.Media;
 
     public class MyFancyView : UserControl
@@ -21,9 +22,16 @@
 
         private void Init()
         {
-            var grid = new System.Windows.Controls.Grid();
-            grid.Background = Brushes.Red;
-            AddChild(grid);
+            try
+            {
+                var grid = new System.Windows.Controls.Grid();
+                grid.Background = Brushes.Red;
+                AddChild(grid);
+            } catch (Exception ex)
+            {
+                Console.WriteLine("==Exception in Init() {0}, trace: {1}==", ex.Message, ex.StackTrace);
+                throw;
+            }
         }
 
         protected override void OnInitialized(EventArgs e)
